55897f55369341accc5ebb2c1729556e087d2e7d
park-junwoo <park.junwoo@navercorp.com>
2022-09-29 00:02:04 +0900

    feat: group기능

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,13 +10,54 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
-mesh.position.set(1.2, -0.9, -2.9);
+const group = new THREE.Group();
+scene.add(group);
 
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "blue" })
+);
+cube2.position.set(2, 0, 0);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "yellow" })
+);
+
+cube3.position.set(1.5, 0.75, -1);
+
+group.add(cube1, cube2, cube3);
+
+group.scale.set(1.2, 1, 0.5);
+group.rotation.reorder("YXZ");
+group.rotation.set(0.5, 1.2, 0);
+
+// position
+// mesh.position.set(-0.7, -1, -4);
+
+// axes
+const axesHelper = new THREE.AxesHelper(1.1);
+
+scene.add(axesHelper);
+
+// scale
+// mesh.scale.x = 3;
+// mesh.scale.y = 2;
+// mesh.scale.z = 3;
+// mesh.scale.set(3, 2, 1);
+
+// // rotate
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.25, 0);
 /**
  * Sizes
  */
@@ -29,7 +70,7 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.set(0.7, 0.5, 2);
 scene.add(camera);
 
 /**
